dd0c615ff79d3433143ce8a1923a6526e3922ac4
joseminaya15 <jose.minayac15@gmail.com>
2018-08-22 16:52:17 -0500

    semiterminado

---
+++ application/views/v_home.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+    <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="description"            content="HPE LAC Value & Growth Channels Warriors - Agosto 7-9, 2018">
+    <meta name="keywords"               content="HPE LAC Value & Growth Channels Warriors">
+    <meta name="robots"                 content="Index,Follow">
+    <meta name="date"                   content="July 23, 2018"/>
+    <meta name="language"               content="es">
+    <meta name="theme-color"            content="#000000">
+	<title>3GT&eacute;cnico</title>
+    <link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.ico">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/css/bootstrap.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>datetimepicker/css/bootstrap-material-datetimepicker.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>owl-carousel/owl.theme.default.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>owl-carousel/owl.carousel.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>metricweb.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>index.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="js-container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand link" href="#home"><img src="<?php echo RUTA_IMG?>logo/logo.png" alt=""></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item"><a class="link" href="#home">Inicio</a></li>
+                    <li class="nav-item"><a class="link" href="#nosotros">Nosotros</a></li>
+                    <li class="nav-item"><a class="link" href="#espacios">Servicios</a></li>
+                    <li class="nav-item"><a class="link" href="#contactanos">Cont&aacute;ctanos</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <section id="home">
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="fondo-responsive fondo1"></div>
+            </div>
+            <div class="item">
+                <div class="fondo-responsive fondo2"></div>
+            </div>
+            <div class="item">
+                <div class="fondo-responsive fondo3"></div>
+            </div>
+        </div>
+        <div class="js-contenido">
+            <h2><span class="first"></span><span class="second"></span></h2>
+            <div class="js-contenido__buttons">
+                <a href="#contactanos" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect link js-button">CONT&Aacute;CTANOS  AHORA</a>
+            </div>
+        </div>
+    </section>
+    <section id="nosotros" class="js-section">
+        <div class="js-container">
+            <h2 class="js-title">Nosotros</h2>
+            <div class="js-information">
+                <div class="js-flex--start">
+                    <div class="js-information__left">
+                        <div class="js-information__left--imagen"></div>
+                    </div>
+                    <div class="js-information__right">
+                        <h2>Servicios t&eacute;cnico profesional a domicilio</h2>
+                        <p>3G T&eacute;cnico es una empresa autorizada para trabajar las marcas en linea blanca, contamos con profesionales especializados para que el electrodom&eacute;stico reciba a tiempo un servicio eficiente.</p>
+                        <p>Realizamos mantenimientos preventivos, reparaci&oacute;n y/o cambios con repuestos 100% originales en marcas europeas, norteamericanas y nacionales.</p>
+                    </div>
+                </div>
+                <div class="js-flex--start second">
+                    <div class="js-information__left">
+                        <div class="js-information__left--imagen one"></div>
+                    </div>
+                    <div class="js-information__right">
+                        <h2>Garant&iacute;a de servicios</h2>
+                        <p>Le ofrecemos el 100% de satisfacci&oacute;n, servicio y garant&iacute;a, de nuestro trabajo.</p>
+                        <p>Nuestra garant&iacute;a por los servicios realizado es por el periodo de 1 a&ntilde;o.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="espacios" class="js-section js-white">
+        <div class="js-container">
+            <h2 class="js-title">Nuestros Servicios</h2>
+            <div class="js-servicios">
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal1"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Reparaci&oacute;n de Lavadoras</h2>
+                    </div>
+                </div>
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal2"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Reparaci&oacute;n de Secadoras</h2>
+                    </div>
+                </div>
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal3"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Reparaci&oacute;n de Cocinas</h2>
+                    </div>
+                </div>
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal4"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Reparaci&oacute;n de Refrigeradoras</h2>
+                    </div>
+                </div>
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal5"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Servicio de Aire Acondicionado</h2>
+                    </div>
+                </div>
+                <div class="mdl-card">
+                    <div class="mdl-card__title principal6"></div>
+                    <div class="mdl-card__supporting-text">
+                        <h2>Servicios El&eacute;ctricos</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="js-subtitle">
+                <h3>Espacio Secundario</h3>
+            </div>
+            <div class="js-espacio--secundario">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario1"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>Deal Room</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario2"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>Waiting Room</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario3"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>Cowork Arqu / D.G KREA</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario4"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>Cowork Arqu / D.G KREA</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario5"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>SS.HH Discapacitados</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="mdl-card">
+                            <div class="mdl-card__title secundario6"></div>
+                            <div class="mdl-card__supporting-text">
+                                <h2>Zona de Lectura KREA</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contactanos" class="js-section">
+        <div class="js-container">
+            <h2 class="js-title">Cont&aacute;ctanos</h2>
+            <div class="js-carousel">
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                         <div class="js-contactanos">
+                            <div class="js-contactanos--left">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3900.97920778097!2d-76.99346688563497!3d-12.11357509142278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2spe!4v1530057031285" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+                            <div class="js-contactanos--right">
+                                <div class="js-flex">
+                                    <i class="mdi mdi-location_on"></i>
+                                    <p>Santiago de Surco</p>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="fa fa-whatsapp"></i>
+                                    <a href="tel:+51991227601">+51 991 227 601</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mdi mdi-call"></i>
+                                    <a href="tel:+13986734">+01 398-6734</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mid mdi-email"></i>
+                                    <a href="mailto:Krea_cowork@gmail.com">giankamo3@gmail.com</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                         <div class="js-contactanos">
+                            <div class="js-contactanos--left">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.439843560589!2d-77.03786503585576!3d-12.082012295845098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8f4f1b8403b%3A0x56ac57368a0ad5f3!2sCentro+Comercial+Arenales%2C+Av.+Arenales%2C+Lince+Lima+14!5e0!3m2!1ses-419!2spe!4v1530057631733" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+                            <div class="js-contactanos--right">
+                                <div class="js-flex">
+                                    <i class="mdi mdi-location_on"></i>
+                                    <p>Jes&uacute;s Mar&iacute;a</p>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="fa fa-whatsapp"></i>
+                                    <a href="tel:+51991227601">+51 991 227 601</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mdi mdi-call"></i>
+                                    <a href="tel:+13986734">+01 398-6734</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mid mdi-email"></i>
+                                    <a href="mailto:Krea_cowork@gmail.com">giankamo3@gmail.com</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                         <div class="js-contactanos">
+                            <div class="js-contactanos--left">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.3052660861636!2d-76.9524557858556!3d-12.091241946019956!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c70549018467%3A0xb7a1e535966b0c26!2sCC+Molina+Plaza!5e0!3m2!1ses-419!2spe!4v1530057341606" width="500" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+                            <div class="js-contactanos--right">
+                                <div class="js-flex">
+                                    <i class="mdi mdi-location_on"></i>
+                                    <p>La Molina</p>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="fa fa-whatsapp"></i>
+                                    <a href="tel:+51991227601">+51 991 227 601</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mdi mdi-call"></i>
+                                    <a href="tel:+13986734">+01 398-6734</a>
+                                </div>
+                                <div class="js-flex">
+                                    <i class="mid mdi-email"></i>
+                                    <a href="mailto:Krea_cowork@gmail.com">giankamo3@gmail.com</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="js-section">
+        <div class="js-container text-center">
+            <p>&copy;Copyright 2018 3GT&eacute;cnico S.A.C.</p>
+        </div>
+    </footer>
+	<script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>moment/moment.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>datetimepicker/js/bootstrap-material-datetimepicker.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>owl-carousel/owl.carousel.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>typedjs/typed.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jsmenu.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jsindex.js?v=<?php echo time();?>"></script>
+    <script type="text/javascript">
+        initButtonCalendarDaysMinToday('llegada');
+        initButtonCalendarDaysMinToday('retorno');
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+            $('select').selectpicker('mobile');
+        } else {
+            $('select').selectpicker();
+        }
+        $(function(){
+            $("h2 .first").typed({
+                strings: ["Hola, hacemos servicio técnico de reparación y mantenimiento de"],             
+                typeSpeed: 80,              
+                backDelay: 1000,                                  
+                showCursor: false,               
+                cursorChar: "|",                            
+                contentType: 'html',       
+                callback: function() {
+                    $("span.second").typed({
+                        strings: [" lavadoras.", " refrigeradoras."," secadoras."," cocinas."],
+                        backDelay: 500,    
+                        typeSpeed: 80,
+                        backSpeed: 0,
+                        loop: true,
+                    });
+
+                },
+            });
+        });
+    </script>
+</body>
+</html>
